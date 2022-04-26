146805b0f6dd25b0ec33212aacdd036e1f216d85
David Oladipo <David.Oladipo@outlook.com>
2022-04-25 20:53:20 -0600

    Added form input,  button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
   const deleteFeedback = (id) => {
@@ -15,7 +16,8 @@ function App() {
     <>
       <Header />
       <div className='container'>
-        <FeedbackStats feedback={feedback}/>
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
